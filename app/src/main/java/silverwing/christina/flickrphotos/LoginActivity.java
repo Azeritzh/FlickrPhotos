080385337674aaf6bb1289d1778cd77c11fd3eff
Azeritzh <azhlander@outlook.com>
2018-11-01 17:04:41 +0100

    Started on FlickrClient and authorization

--- app/src/main/java/silverwing/christina/flickrphotos/LoginActivity.java
+++ app/src/main/java/silverwing/christina/flickrphotos/LoginActivity.java
@@ -1,25 +1,33 @@
 package silverwing.christina.flickrphotos;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+import com.codepath.oauth.OAuthLoginActivity;
+
+public class LoginActivity extends OAuthLoginActivity<FlickrClient> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button loginButton = (Button)findViewById(R.id.login_button);
+        /*Button loginButton = (Button)findViewById(R.id.login_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent loginIntent = new Intent(getApplicationContext(), MenuActivity.class);
                 startActivity(loginIntent);
             }
-        });
+        });*/
+    }
+
+    @Override
+    public void onLoginSuccess() {
+
+    }
+
+    @Override
+    public void onLoginFailure(Exception e) {
+
     }
 }
